d6cae2b21b1ca45aa2c13042994da22d2ae8acb5
Sohaib <sohaibmontaki370@gmail.com>
2026-05-16 13:36:15 +0100

    création du NavbarComponent et du CustomersComponent avec liste client

---
+++ frontend/digital-banking-web/src/app/customers/customers.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Customer } from '../model/model';
+import { CustomerService } from '../services/customer.service';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+  customers!: Observable<Array<Customer>>;
+  errorMessage!: string;
+  searchFormGroup!: FormGroup;
+
+  constructor(
+    private customerService: CustomerService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control('')
+    });
+    this.handleSearchCustomers();
+  }
+
+  handleSearchCustomers() {
+    let kw = this.searchFormGroup?.value.keyword;
+    this.customers = this.customerService.searchCustomers(kw);
+  }
+
+  handleDeleteCustomer(c: Customer) {
+    let conf = confirm("Are you sure to delete this Customer?");
+    if (!conf) return;
+    this.customerService.deleteCustomer(c.id).subscribe({
+      next: () => {
+        this.handleSearchCustomers();
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+
+  handleCustomerAccounts(customer: Customer) {
+    this.router.navigateByUrl("/customer-accounts/" + customer.id, { state: customer });
+  }
+}
