55ee1842787da55e9e1c055d748adbde774f5b22
Sohaib <sohaibmontaki370@gmail.com>
2026-05-16 16:28:01 +0100

    feat: ajout du formulaire NewCustomer et configuration du routing Angular CustomerAccounts - affichage des comptes d'un client

--- frontend/digital-banking-web/src/app/app-routing.module.ts
+++ frontend/digital-banking-web/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomersComponent } from './customers/customers.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { CustomerAccountsComponent } from './customer-accounts/customer-accounts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/customers', pathMatch: 'full' },
+  { path: 'customers', component: CustomersComponent },
+  { path: 'new-customer', component: NewCustomerComponent },
+  { path: 'accounts', component: AccountsComponent },
+  { path: 'customer-accounts/:id', component: CustomerAccountsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/digital-banking-web/src/app/new-customer/new-customer.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomerService } from '../services/customer.service';
+
+@Component({
+  selector: 'app-new-customer',
+  templateUrl: './new-customer.component.html',
+  styleUrls: ['./new-customer.component.css']
+})
+export class NewCustomerComponent implements OnInit {
+  newCustomerFormGroup!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private customerService: CustomerService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.newCustomerFormGroup = this.fb.group({
+      name: this.fb.control('', [Validators.required, Validators.minLength(4)]),
+      email: this.fb.control('', [Validators.required, Validators.email])
+    });
+  }
+
+  handleSaveCustomer() {
+    let customer = this.newCustomerFormGroup.value;
+    this.customerService.saveCustomer(customer).subscribe({
+      next: () => {
+        alert("Customer saved successfully!");
+        this.router.navigateByUrl("/customers");
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+}
